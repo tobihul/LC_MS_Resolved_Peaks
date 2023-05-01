3c25255b676070e90dcc1c4d3cde2f3c85f1554e
Tobias Hulleman <107274608+tobihul@users.noreply.github.com>
2023-05-01 10:10:33 +0200

    Update ProjectTemp.jl

--- src/ProjectTemp.jl
+++ src/ProjectTemp.jl
@@ -3,7 +3,7 @@ module ProjectTemp2
 include("/Users/tobias/.julia/dev/ProjectTemp2/src/Functionalized and sped up code.jl")
 
     export mass_align, resolutions, unresolved_per_window_Rt_and_MS, window_split_Rt, Peaks_p_window,
-        plot_heatmap
+        plot_heatmap, gradient_curve, normalize_lc_ms, surface_voronoi, load_and_prep_data
 end
 
 
