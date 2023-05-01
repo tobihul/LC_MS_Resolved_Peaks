41b1c62750ceb5d921b8eb663db5dcfce48c21f4
Tobias Hulleman <107274608+tobihul@users.noreply.github.com>
2023-05-01 10:15:49 +0200

    Update LC_MS_Resolutions.jl

--- src/LC_MS_Resolutions.jl
+++ src/LC_MS_Resolutions.jl
@@ -1,6 +1,6 @@
 module LC_MS_Resolutions
 
-include("/Users/tobias/.julia/dev/ProjectTemp2/src/Functionalized and sped up code.jl")
+include("Functionalized and sped up code.jl")
 
     export mass_align, resolutions, unresolved_per_window_Rt_and_MS, window_split_Rt, Peaks_p_window,
         plot_heatmap, gradient_curve, normalize_lc_ms, surface_voronoi, load_and_prep_data
