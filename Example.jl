a277e0e7168a25afe763460649f1a6092f39f1ae
Tobias Hulleman <107274608+tobihul@users.noreply.github.com>
2023-05-09 10:30:15 +0200

    Update Example.jl

--- Example.jl
+++ Example.jl
@@ -7,21 +7,17 @@ using LC_MS_Resolutions
 #previous run:
 #New MZXML file:
 #Set filename
-pathin = "/Users/tobias/Documents/Tobi_data/20230324_pest_stds_SBC18_short_2" 
-pathin = "/Users/tobias/Documents/Tobi_data/20230327_pest_stds_BonusRP" 
-pathin = "/Users/tobias/Documents/Tobi_data/20230327_pest_stds_PFP" 
-filenames = ["Std_04.mzXML"]
+pathin = "path_to_your_LC_MS_file" 
+filenames = ["name_of_your_file.mzXML"]
 mz_thresh = [0, 0] #Sets threshold for mz values
 int_thresh = 500 #Remove most of the noise
 #Load the file for your gradient here
-gradient_data = CSV.read("/Users/tobias/Documents/Tobi_data/Gradients/Gradient short_2.csv", DataFrame)
-gradient_data = CSV.read("/Users/tobias/Documents/Tobi_data/Gradients/BONUS RP Gradient.csv", DataFrame)
-gradient_data = CSV.read("/Users/tobias/Documents/Tobi_data/Gradients/POROSHELL PFP.csv", DataFrame)
+gradient_data = CSV.read("path_to_your_gradient/name_of_your_gradient.csv", DataFrame)
 
 #Import MS data
 GC.gc()
 
-@time mz_val, mz_int, t0, t_end, m, path, msModel, msIonisation, msManufacturer,
+mz_val, mz_int, t0, t_end, m, path, msModel, msIonisation, msManufacturer,
 polarity, Rt = LC_MS_Resolutions.MS_Import.import_files_MS1(pathin, filenames, mz_thresh, int_thresh)
 FileName = m[1]
 #Adjust the settings for SAFD here
@@ -35,8 +31,8 @@ sig_inc_thresh=5
 S2N=3 #minimum signal to noise ratio for SAFD to take data as a feature
 min_peak_w_s=3
 GC.gc()
-#Run SAFD (this may take some time depending on n iterations)
-@time rep_table, SAFD_output = LC_MS_Resolutions.SAFD.safd_s3D(mz_val, mz_int, Rt, FileName, path, max_numb_iter,
+#Run SAFD (this may take some time depending on n iterations and min_int)
+rep_table, SAFD_output = LC_MS_Resolutions.SAFD.safd_s3D(mz_val, mz_int, Rt, FileName, path, max_numb_iter,
 max_t_peak_w, res, min_ms_w, r_thresh, min_int, sig_inc_thresh, S2N, min_peak_w_s)
 
 #Componentization of features
@@ -59,16 +55,19 @@ chrom=LC_MS_Resolutions.MS_Import.import_files(pathin,filenames,mz_thresh,int_th
 ## For only MS1
 SAFD_output = LC_MS_Resolutions.CompCreate.comp_ms1(chrom,path2features,mass_win_per,ret_win_per,r_thresh,delta_mass, min_int)
 
+wind_size = 12 #define the number of windows to split the Rt domain in
+resolution = 1.5 #define the accepted resolution for two features to be considered resolved
+
 #Align masses and run resolutions algorithm
 unique_mz_values, plot_matrix = mass_align(Rt, mz_val, mz_int)
-results, colors, df, gradient = @time unresolved_per_window_Rt_and_MS(Rt, SAFD_output, 12, 1.5, gradient_data)
+colors, results, gradient = unresolved_per_window_Rt_and_MS(Rt, SAFD_output, wind_size, resolution, gradient_data)
 df   #This DataFrame contains all the results
 #Save the dataframe as CSV
-CSV.write("/Users/tobias/Library/CloudStorage/OneDrive-UvA/Research project UvA/Figures and dataframes from US data/PFP, mix 4.csv" ,df)
+CSV.write("Path_to_where_you_want_your_results/name_of_your_file.csv" ,results)
 #Plot the heatmap with features and windows
 plot_heatmap(SAFD_output, Rt, unique_mz_values, plot_matrix, 12, gradient_data, colors, filenames, pathin)
 
-savefig("/Users/tobias/Library/CloudStorage/OneDrive-UvA/Research project UvA/Figures and dataframes from US data/PFP, mix 4.png")
+savefig("Path_to_where_you_want_your_figure/name_of_your_figure.png") #save the figure as png, pdf or svg with the extension after .
 
 #################
 #END 
