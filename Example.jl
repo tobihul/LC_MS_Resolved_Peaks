faeeb6348c4c4e4e0177a42b32a87083f3e77be8
Tobias Hulleman <107274608+tobihul@users.noreply.github.com>
2023-05-09 10:49:13 +0200

    Update Example.jl

--- Example.jl
+++ Example.jl
@@ -12,7 +12,7 @@ filenames = ["name_of_your_file.mzXML"]
 mz_thresh = [0, 0] #Sets threshold for mz values
 int_thresh = 500 #Remove most of the noise
 #Load the file for your gradient here
-gradient_data = CSV.read("path_to_your_gradient/name_of_your_gradient.csv", DataFrame)
+gradient_data = LC_MS_Resolutions.CSV.read("path_to_your_gradient/name_of_your_gradient.csv", LC_MS_Resolutions.DataFrame)
 
 #Import MS data
 GC.gc()
@@ -63,7 +63,7 @@ unique_mz_values, plot_matrix = mass_align(Rt, mz_val, mz_int)
 colors, results, gradient = unresolved_per_window_Rt_and_MS(Rt, SAFD_output, wind_size, resolution, gradient_data)
 df   #This DataFrame contains all the results
 #Save the dataframe as CSV
-CSV.write("Path_to_where_you_want_your_results/name_of_your_file.csv" ,results)
+LC_MS_Resolutions.CSV.write("Path_to_where_you_want_your_results/name_of_your_file.csv" ,results)
 #Plot the heatmap with features and windows
 plot_heatmap(SAFD_output, Rt, unique_mz_values, plot_matrix, 12, gradient_data, colors, filenames, pathin)
 
