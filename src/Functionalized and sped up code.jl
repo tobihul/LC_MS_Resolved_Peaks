2bc169f72cd4642418fd682cc7c7d107e443947b
Tobias Hulleman <tehulleman@gmail.com>
2023-05-15 11:31:13 +0200

    Small fix where colors lenth would be wrong

--- src/Functionalized and sped up code.jl
+++ src/Functionalized and sped up code.jl
@@ -161,7 +161,6 @@ function unresolved_per_window_Rt_and_MS(Rt::Vector{Float32}, SAFD_output::DataF
             #Calculating the orthogonality of the window with weights
             Rt_norm, MS_norm = normalize_lc_ms(SAFD_output[Peaks_per_window[i],:])
             tot_unresolved_final[i,7] = surface_voronoi(Rt_norm, MS_norm, 2.2) * Mass_weights[i]
-            
             #############
             res_Rt, res_M = resolutions(SAFD_output[Peaks_per_window[i][1]:Peaks_per_window[i][end], :])
             #Initialize the bitmatrixes
@@ -245,7 +244,7 @@ function Peaks_p_window(wind_size::Int64, Rt::Vector{Float32}, SAFD_output::Data
     Peaks_per_window = Vector{Vector{Int64}}(undef, wind_size)
     pos::Int64 = 1
     #Divide the Rt domain in n equal parts 
-    time_diff = (Rt[end] - Rt[1]) / wind_size
+    time_diff = (Rt[end]) / wind_size
     SAFD_Rt::Vector{Float32} = SAFD_output[:, 4]
     #This loop checks in which of the defined windows the SAFD feature is by the center point of the feature
     for i = 1:wind_size
@@ -449,3 +448,65 @@ function Resolved_peaks_algorithm(pathin, filenames,gradient_data, wind_size, ac
     savefig(path_for_image)
     println("Your image has been saved to $path_for_image")
 end
+pathin = "/Users/tobias/Documents/Tobi_data/20230327_pest_stds_BonusRP" 
+filenames = ["Std_05.mzXML"]
+mz_thresh = [0, 0] #Sets threshold for mz values
+int_thresh = 500 #Remove most of the noise
+#Load the file for your gradient here
+gradient_data = CSV.read("/Users/tobias/Documents/Tobi_data/Gradients/BONUS RP Gradient.csv", DataFrame)
+
+#Import MS data
+GC.gc()
+
+mz_val, mz_int, t0, t_end, m, path, msModel, msIonisation, msManufacturer,
+polarity, Rt = MS_Import.import_files_MS1(pathin, filenames, mz_thresh, int_thresh)
+FileName = m[1]
+#Adjust the settings for SAFD here
+max_numb_iter = 2000 #Roughly the number of features that will be found, if there are less than n_iter it will stop automatically
+max_t_peak_w=300 # or 20
+res=20000
+min_ms_w=0.02
+r_thresh=0.9 #How well the fitted gaussian overlaps the original peak
+min_int=10000
+sig_inc_thresh=5
+S2N=3 #minimum signal to noise ratio for SAFD to take data as a feature
+min_peak_w_s=3
+GC.gc()
+#Run SAFD (this may take some time depending on n iterations and min_int)
+rep_table, SAFD_output = SAFD.safd_s3D(mz_val, mz_int, Rt, FileName, path, max_numb_iter,
+max_t_peak_w, res, min_ms_w, r_thresh, min_int, sig_inc_thresh, S2N, min_peak_w_s)
+
+#Componentization of features
+#Add the file location of the features reported by SAFD to run CompCreate
+#The report file should be located wherever your MZXML file is
+#The following code fetches the name of the file automatically given that it is stored in the same place as your dataframe
+#If you stored the report in a different location, create a variable called path2features leading to your report csv using CSV.read("yourpath.csv",DataFrame)
+basename_pathin = basename(pathin)
+filename_no_ext = splitext(filenames[1])[1]
+path2features = joinpath(pathin*"/"*filename_no_ext*"_report.csv")
+mass_win_per=0.8
+ret_win_per=0.5
+r_thresh=0.9
+delta_mass=0.004
+min_int = 750
+
+chrom=MS_Import.import_files(pathin,filenames,mz_thresh,int_thresh)
+
+1897322032
+## For only MS1
+SAFD_output = CompCreate.comp_ms1(chrom,path2features,mass_win_per,ret_win_per,r_thresh,delta_mass, min_int)
+
+wind_size = 12 #define the number of windows to split the Rt domain in
+resolution = 1.5 #define the accepted resolution for two features to be considered resolved
+alpha = 1 #define the weight of the unresolved peaks % score in the final score 
+beta = 1 #Define the weight of the surface coverage score in the final score
+#The weights can be lower than 1 to reduce the weights or hogher than 1 to increase the weights
+
+#Align masses and run resolutions algorithm
+unique_mz_values, plot_matrix = mass_align(Rt, mz_val, mz_int)
+colors, results, gradient = unresolved_per_window_Rt_and_MS(Rt, SAFD_output, wind_size, resolution, gradient_data, alpha, beta)
+colors  #This DataFrame contains all the results
+#Save the dataframe as CSV
+CSV.write("Path_to_where_you_want_your_results/name_of_your_file.csv" ,results)
+#Plot the heatmap with features and windows
+plot_heatmap(SAFD_output, Rt, unique_mz_values, plot_matrix, wind_size, gradient_data, colors, filenames, pathin)
